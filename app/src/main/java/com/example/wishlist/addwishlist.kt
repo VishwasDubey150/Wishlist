6866f61ab41542deb1257f2ea24909ccda4b9c08
Vishwas <VishwasDubey150@gmail.com>
2022-08-08 18:18:25 +0530

    third commit

--- app/src/main/java/com/example/wishlist/addwishlist.kt
+++ app/src/main/java/com/example/wishlist/addwishlist.kt
@@ -1,12 +1,32 @@
 package com.example.wishlist
 
+import android.app.DatePickerDialog
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import java.util.*
 
 class addwishlist : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addwishlist)
         supportActionBar?.hide()
+
+    }
+
+    fun date(view: android.view.View) {
+        val date=findViewById<Button>(R.id.date)
+        val mycalender=Calendar.getInstance()
+        val year=mycalender.get(Calendar.YEAR)
+        val month=mycalender.get(Calendar.MONTH)
+        val day=mycalender.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,DatePickerDialog.OnDateSetListener {
+                view, Syear, Smonth, SdayOfMonth ->
+            val selected="$SdayOfMonth/${Smonth+1}/$Syear"
+            date.setText(selected)
+        },year,month,day).show()
     }
 }
